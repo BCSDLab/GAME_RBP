265f6ac6d24467d91092b680ced9622c63f1ceee
SIRYU <siryu2409@gmail.com>
2020-04-11 23:11:39 +0900

    Add google play service Add Firebase Library Add Title Scene
    Former-commit-id: d3b05404c45c058e3f35c12b015e95a96e81c726

---
+++ RhythmBeatPlay/Assets/Script/TitleScene/TitleUIManager.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class TitleUIManager : MonoBehaviour
+{
+    public Button settingButton;
+    public GameObject MainMenu;
+    public GameObject SubMenu;
+    public Text tokenText;
+    public Text DriveToken;
+
+    private void Awake()
+    {
+        // refreshUI(PlayCloudDataManager.Instance.isAuthenticated);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayCloudDataManager.Instance.loginEvent.AddListener(refreshUI);
+
+        getDriveToken();
+    }
+
+    // Update is called once per frame
+    int tick = 0;
+    int temptok = 0;
+    int sec = 0;
+    void Update()
+    {
+        tokenText.text = "Token : " + DataManager.Instance.token;
+        if (++tick > 60)
+        {
+            tick = 0;
+            sec += 1;
+        }
+    }
+
+    public void getDriveToken()
+    {
+        if (!Application.isEditor)
+        {
+            if (DataManager.Instance.IsConnected)
+            {
+                PlayCloudDataManager.Instance.LoadFromCloud((string dataToLoad) => { DataManager.Instance.token = int.Parse(dataToLoad); });
+                DriveToken.text = "Sec : "+ sec + ", "+ "Drive Token : " + DataManager.Instance.token;
+            }
+        }
+    }
+
+    public void refreshUI(bool isSigned)
+    {
+
+        if (isSigned)
+        {
+            OnLoginUIRefresh(isSigned);
+        }
+        else
+        {
+            OnLogOutUIRefresh(isSigned);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        PlayCloudDataManager.Instance.loginEvent.RemoveListener(refreshUI);
+    }
+
+    private void OnLogOutUIRefresh(bool isSigned)
+    {
+        MainMenu.SetActive(true);
+    }
+
+    private void OnLoginUIRefresh(bool isSigned)
+    {
+        MainMenu.SetActive(false);
+    }
+
+    public void ToStageScene()
+    {
+        if (PlayCloudDataManager.Instance.isAuthenticated)
+        {
+            SceneManager.LoadScene("StageScene");
+        }
+    }
+
+    public void SettingsButtonClicked()
+    {
+        SubMenu.transform.localPosition = new Vector3(0f, 0f, 0f);
+    }
+
+    public void SettingsButtonCanceled()
+    {
+        SubMenu.transform.localPosition = new Vector3(1920f, 0f, 0f);
+    }
+}
