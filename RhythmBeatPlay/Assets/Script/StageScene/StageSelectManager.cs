8f6dc6c9472f3f32a1904dcbf1122603efcf7d3a
YunSangun <tkaltk123@gmail.com>
2020-04-29 16:33:11 +0900

    [DONE][윤상운][RBP-021] 씬 로드 UI 프로토타입 생성
    - https://trello.com/c/VhCPOjoE/252-rbp-021-%EA%B2%8C%EC%9E%84-%EC%94%AC-%EB%A1%9C%EB%94%A9%ED%99%94%EB%A9%B4-%EC%B6%94%EA%B0%80
    - https://wergia.tistory.com/m/194 글을 참고하여 프로그레스바와 씬 로드 액션을 포함하는 씬 로드 UI의 초기버전 작성

--- RhythmBeatPlay/Assets/Script/StageScene/StageSelectManager.cs
+++ RhythmBeatPlay/Assets/Script/StageScene/StageSelectManager.cs
@@ -7,6 +7,17 @@ public class StageSelectManager : SelectManager
         base.selectedObjNumber = DataObject.inst.stageNumber;
         base.Start();
     }
+    protected override void Update()
+    {
+        base.Update();
+        if (Input.GetKeyDown(KeyCode.Space))
+
+        {
+
+            SceneLoader.Instance.LoadScene("MusicScene");
+
+        }
+    }
     protected override void objectSelect()
     {
         DataObject.inst.stageNumber = base.selectedObjNumber;
