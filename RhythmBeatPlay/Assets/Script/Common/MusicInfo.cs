2fec3a974d3c0c544be5b58819b519c5724c99b7
YunSangun <tkaltk123@gmail.com>
2020-09-01 14:44:30 +0900

    [RBP-039] MusicSelectScene-GameScene 연동
    - https://trello.com/c/FMxcUskj/310-rbp-039-musicselectscene-gamescene-%EC%97%B0%EB%8F%99
    - 오디오클립, 제목, 아티스트, 앨범, noteCount, bpm, musicLengthSecond, titleSprite, backgroundSprite 로 구성된 MusicInfo 스크립트
    - MusicInfo 스크립트가 부착된 Music Prefab
    - MusicInfoManager를 통해 music code 또는 스테이지와 음악 순서번호로 MusicInfo load
    - 게임씬에서 stage와 music 순서번호 확인은 DataManager의 stageNumber, musicNumber

---
+++ RhythmBeatPlay/Assets/Script/Common/MusicInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicInfo : MonoBehaviour
+{
+    [SerializeField]
+    AudioClip music;
+
+    [SerializeField]
+    string title;
+
+    [SerializeField]
+    string artist;
+
+    [SerializeField]
+    string album;
+
+    [SerializeField]
+    int noteCount;
+
+    [SerializeField]
+    int bpm;
+
+    [SerializeField]
+    float musicLengthSecond;
+
+    // must be scaled 1:1
+    [SerializeField]
+    Sprite titleSprite;
+
+    // must be scaled 16:9
+    [SerializeField]
+    Sprite backgroundSprite;
+}
